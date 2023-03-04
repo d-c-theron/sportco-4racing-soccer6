9d584938af558838506813aa673f2654af1a55c5
David Theron <david@sportco.co.za>
2023-03-04 11:22:12 +0200

    First draft showing matches on home widget

--- src/components/PoolItem.js
+++ src/components/PoolItem.js
@@ -1,15 +1,32 @@
 import React from "react";
+import MatchSummary from "./MatchSummary";
 
 const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const monthName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-const getPrettyDate = (d) => {
+const getPrettyDate = (programDate) => {
+  let d = new Date(programDate);
   return weekday[d.getDay()] + " " + d.getDate() + " " + monthName[d.getMonth()];
 };
 
 const PoolItem = ({ item }) => {
   return (
-    <div class="pooldateindicator4R">
-      {getPrettyDate(new Date(item.programDate))} - {item.PartitionKey}
+    <div>
+      <div className="pooldateindicator4R">
+        {getPrettyDate(item.programDate)} - {item.PartitionKey}
+      </div>
+      <table className="pooltable">
+        <tbody>
+          <tr>
+            <th>Match</th>
+            <th>Team A</th>
+            <td style={{ textAlign: "center" }}>v</td>
+            <th className="righttext">Team B</th>
+          </tr>
+          {JSON.parse(item.matches).map((match) => (
+            <MatchSummary item={match} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
