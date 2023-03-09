b1fd7c08af14da1d3f49859f3233aefd180362e5
David Theron <david@sportco.co.za>
2023-03-09 18:24:19 +0200

    Switch front page to Summary Pools

--- api/GetSummaryPools/index.js
+++ api/GetSummaryPools/index.js
@@ -4,7 +4,7 @@ const { queryEntities } = require("../services/table-services");
 module.exports = async function (context, req) {
   try {
     var query = new azure.TableQuery();
-    const result = await queryEntities("pools", query);
+    const result = await queryEntities("summarypools", query);
     context.res = {
       body: result,
     };
