1aaeb3cb7f35f31fb70e4ec020d8e422dded5492
David Theron <david@sportco.co.za>
2023-03-29 12:38:45 +0200

    Added Lower Home Page furniture

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,6 +7,33 @@ const HomePage = () => {
     <div>
       <PoolsWidget />
       <ArticlesElement />
+      <div className="row third">
+        <div>
+          <a href="https://aztabstorage.blob.core.windows.net/tabonline-blob/FieldsPDF/SportsExpress/SportsExpress.pdf">
+            <img alt="Sports Express" src="/images/Sports_Express.jpg" style={{ width: "100%", height: "10%" }} />
+          </a>
+          <br />
+          <br />
+        </div>
+      </div>
+      <div className="row fourth">
+        <div clclassNameass="large-8 columns">
+          <div className="large-6 columns smallpaddingright">
+            <div className="sectionheader nospace">General Info</div>
+            <div className="flex-video">
+              <iframe width={252} height={206} frameBorder={0} src="https://www.youtube.com/embed/2sWEJb_6jZE?rel=0"></iframe>
+            </div>
+          </div>
+
+          <div className="large-6 columns smallpaddingleft">
+            <div className="sectionheader nospace">PROMO</div>
+            <div className="flex-video">
+              <iframe width={252} height={206} frameBorder={0} src="https://www.youtube.com/embed/WjWXNPK0G_Y?rel=0"></iframe>
+            </div>
+          </div>
+        </div>
+        <div className="large-4 columns" style={{ paddingLeft: "3px", paddingRight: "0" }}></div>
+      </div>
     </div>
   );
 };
